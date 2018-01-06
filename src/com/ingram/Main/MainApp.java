1fa0e5fb0d493e5505cc7590032e806c80a0c153
Nicholas-Ingram <ingram.nicholas.a@gmail.com>
2018-01-06 14:39:53 -0600

    Sarted messing with JFrame window

--- src/com/ingram/Main/MainApp.java
+++ src/com/ingram/Main/MainApp.java
@@ -1,30 +1,14 @@
 package com.ingram.Main;
 
-import com.ingram.Data.FetchCardNames;
-
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class MainApp {
     
     private JPanel panelMain;
-    private JButton btnTestFetch;
-    
-    public MainApp ()
-    {
-        btnTestFetch.addActionListener (new ActionListener () {
-            @Override
-            public void actionPerformed (ActionEvent e)
-            {
-                FetchCardNames.fetchCardNames ();
-            }
-        });
-    }
     
     public static void main (String[] args)
     {
-        JFrame mainFrame = new JFrame ("MainApp");
+        JFrame mainFrame = new JFrame ("Yu-Gi-Oh! Card Manager");
         mainFrame.setContentPane (new MainApp ().panelMain);
         mainFrame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
         mainFrame.pack ();
